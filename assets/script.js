ac03d0bba5baf0aad5a1e6d80a1ec21f023e467d
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-06 01:34:34 -0700

    question 1

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@ var timeEl = document.querySelector(".time");
 
 // select elements by id
 var startBtn = document.querySelector("#startBtn");
+var question = document.querySelector("#title");
+var answers = document.querySelector("#answers");
+var instructions = document.querySelector("#instructions");
 
 var secondsLeft = 60;
 
@@ -20,6 +23,33 @@ startBtn.addEventListener("click", function setTime() {
       // calls function to show final score and enter initials
     }
   }, 1000);
+});
+
+startBtn.addEventListener("click", function firstQuestion() {
+  question.textContent = "Commonly used data types DO NOT include:";
+
+  instructions.remove();
+
+  var listEl = document.createElement("ul");
+  var strings = document.createElement("li");
+  var booleans = document.createElement("li");
+  var alerts = document.createElement("li");
+  var numbers = document.createElement("li");
+
+  listEl.classList.add("choices");
+
+  strings.textContent = "1. Strings";
+  booleans.textContent = "2. Booleans";
+  alerts.textContent = "3. Alerts";
+  numbers.textContent = "4. Numbers";
+
+  answers.appendChild(listEl);
+  listEl.appendChild(strings);
+  listEl.appendChild(booleans);
+  listEl.appendChild(alerts);
+  listEl.appendChild(numbers);
+
+  startBtn.remove();
 });
 
 // save initials and score
